7ee6ffa3dd7927a4fedfe0e8915d64f4cd2a8679
Matias Zuber Schachner <matias.zs.1967@gmail.com>
2022-08-24 16:39:47 -0300

    Proyecto parcial terminado, interaccion entre JS y HTML

--- main.js
+++ main.js
@@ -1,159 +1,261 @@
 //Crear la clase "Gastos"
 class Gasto {
-    constructor(nombre, monto) {
-        //this.tipo = tipo;
+    constructor(id, nombre, monto) {
+        this.id = id;
         this.nombre = nombre;
         this.monto = monto;
     }
     elemCreado(){
-        console.log("Se ha creado un objeto: Gasto" + "\nRazon: " + this.nombre + "\nMonto: " + this.monto)
+        console.log("Se ha creado un objeto: Gasto" + "\nID: " + this.id + "\nRazon: " + this.nombre + "\nMonto: " + this.monto)
     }
+}
+//inicializar variables globales
+let resumenMovAct = [];
+let resumenMovTot = [];
+let flagInicio = false;
+let tablaResumen = document.getElementById("tabla1");
+
+//Detectar evento para iniciar la carga de movimientos
+let btnIng = document.getElementById("btnIngresarMov");
+btnIng.onclick = () =>{crearPanelIng()}
+let btnIngSec = document.getElementById("btnIngSecundario");
+btnIngSec.onclick = () =>{crearPanelIng()}
+
+//Mostrar (crear) panel para ingresar datos
+function crearPanelIng() {
+    window.location.href = '#nvoMov'
+    let contenedorCarga = document.getElementById("formDeCarga")
+    contenedorCarga.innerHTML = `<form action="" id="formularioCarga">
+                                <input class="panelIng" type="text" placeholder="Nombre de la Transaccion" id="nombreForm">
+                                <input class="panelIng" type="number" placeholder="Monto de la transaccion" id="montoForm">
+                                <input class="botonIng" type="submit" value="Enviar">
+                            </form>`
+    let formCarga = document.getElementById("formularioCarga");
+    formCarga.addEventListener("submit", nvoGasto)
 }
 
 //funcion para crear nuevos gastos
-function nvoGasto (collec) {
-    let nom = prompt("Ingrese el motivo del gasto: ");
-    let monto = parseInt(prompt("Ingrese el monto gastado: "));
-    if (Number(monto) == monto) {
-        const gasto1 = new Gasto (nom, monto);
-        collec.push(gasto1);
-        gasto1.elemCreado()
-    }
-    else {
-        alert("No se pudo cargar el resultado, intente nuevamente")
+function nvoGasto (e) {
+    e.preventDefault();
+    // let formulario = e.target;
+    let id = resumenMovTot.length
+    let inputNom = document.getElementById("nombreForm");
+    let nom = inputNom.value;
+    let inputMonto = document.getElementById("montoForm");
+    let monto = inputMonto.value;
+    const gasto1 = new Gasto (id, nom, monto);
+    resumenMovAct.push(gasto1);
+    resumenMovTot.push(gasto1)
+    gasto1.elemCreado();
+    mostrarResumen();
+    deshacerPanelIng()
+    window.location.href = '#resumen';
+}
+
+//Recargar el resumen con los movimientos actualizados
+function mostrarResumen () {
+    let tablaResumen = document.getElementById("tabla1");
+    tablaResumen.innerHTML = "";  
+    for (const mov of resumenMovAct) {
+        filaResumen = document.createElement("tr");
+        filaResumen.innerHTML = `<td>${mov.id}</td>
+                                <td>${mov.nombre}</td>
+                                <td>${mov.monto}</td>`;
+        tablaResumen.append(filaResumen);
+        console.log(resumenMovAct.length);
     }
 }
 
-// Funcion para mostrar el resumen actual
-function verResumen (array) {
-    let index = 0;
-    for (const movimientos of array) { 
-        console.log(index + ")" + "\nRazon: " + movimientos.nombre + "\nMonto: " + movimientos.monto);
-        index++;
-    }
+//Eliminar el panel para ingresar movimientos
+function deshacerPanelIng(){
+    let contenedorCarga = document.getElementById("formDeCarga")
+    contenedorCarga.innerHTML = "";
 }
 
-//Funcion para eliminar un resumen
-function eliminarResumen (array) { 
-    del = prompt("Ingrese el numero de orden del gasto que quiere eliminar")
-    if (array.lenght < del) {
-        alert("El numero ingresado no es valido")
-    }else{
-        array.splice (del, 1);
-    }
+//Detectar evento para crear el panel de modificacion
+let btnMod = document.getElementById("btnModificarMov");
+btnMod.onclick = () =>{crearPanelMod()}
+let btnModSec = document.getElementById("btnModSecundario");
+btnModSec.onclick = () =>{crearPanelMod()}
+let flagIdMod = -1;
+
+//crear el panel de modificacion
+function crearPanelMod() {
+    window.location.href = '#modMov'
+    let contenedorMod = document.getElementById("formMod")
+    contenedorMod.innerHTML = "";
+    contenedorMod.innerHTML = `<input type="number" class="panelMod" id="findIdMod" placeholder="Buscar por ID">`;
+    let inputModId = document.getElementById("findIdMod");
+    inputModId.onchange =  () => {filtResultadosMod(inputModId.value)}
 }
 
-// Funcion para eliminar un gasto
-function modificarResumen (array) {
-    opc = prompt("Para agregar un nuevo gasto, presione 1" + "\nPara eliminar un gasto, presione 2" + "\nPara salir, presione 0")
-    while (opc != "0") { 
-        switch (opc) {
-            case "1":
-                nvoGasto (array);
-                verResumen (array);
-                break
-            case "2":
-                eliminarResumen (array);
-                verResumen (array);
-                break
-            default:
-                alert("No se ha ingresado una opcion correcta")
-                break
-            }
-        opc = prompt("Para agregar un nuevo gasto, presione 1" + "\nPara eliminar un gasto, presione 2" + "\nPara salir, presione 0");
+
+//Buscar el objeto por su Id y mostrarlo en una tabla, habilitando el panel de modificacion
+function filtResultadosMod(valor){
+    flagIdMod = valor;
+    busqueda = resumenMovAct.find((el) => el.id === Number(valor));
+    let panelMod = document.getElementById("formularioMod");
+    panelMod.innerHTML = "";
+    let tableTopMod = document.getElementById("tableModTop");
+    tableTopMod.innerHTML = "";
+    let tablaResultadosMod = document.getElementById("tabla2");
+    tablaResultadosMod.innerHTML = ""; 
+    let contenedorMod = document.getElementById("formMod")
+    contenedorMod.innerHTML = ""
+    if (resumenMovAct.includes(busqueda)) {
+        let tableTopMod = document.getElementById("tableModTop");
+        let filaTopMod = document.createElement("tr");
+        filaTopMod.innerHTML = "";
+        filaTopMod.innerHTML = `<td>ID</td>
+                            <td>NOMBRE</td>
+                            <td>MONTO</td>`;
+        tableTopMod.append(filaTopMod);
+        let filaResultado = document.createElement("tr");
+        filaResultado.innerHTML = `<td>${busqueda.id}</td>
+                                    <td>${busqueda.nombre}</td>
+                                    <td>${busqueda.monto}</td>`;
+        tablaResultadosMod.appendChild(filaResultado);
+        modificarMov(busqueda)
+    } else {
+        let contenedorMod = document.getElementById("formMod")
+        contenedorMod.innerHTML = `<p class="text-center">El valor Ingresado no existe o no es valido</p>`;
     }
-}
-
-//funciones para procesar los gastos
-
-function calcularGasto (valor1, valor2) {
-    return Number(valor1) + Number(valor2);
-} 
-
-function procesarGastos (array1, array2, array3){
-    for (const movimientos of array1) {
-        if (gasto + movimientos.monto <= limite) {
-            gasto = calcularGasto (gasto, movimientos.monto);
-            array2.push(movimientos);
-        }
-        else{
-            array3.push(movimientos);
-        }
-    }
-    console.log("Gastos procesados dentro del Limite: ");
-    for (const movimientos of array2) {
-        console.log("Razon: " + movimientos.nombre + "\nMonto: $" + movimientos.monto)
-    }
-    console.log("Gastos que no pudieron procesarse:");
-    for (const movimientos of array3) {
-        console.log("Razon: " + movimientos.nombre + "\nMonto: $" + movimientos.monto)
-    }
-}
-
-//funcion para mostrar los gastos en el HTML
-function mostrarGastos (array) {
-    for (const movimientos of array) {
-        let container = document.createElement("div");
-        //Definimos el innerHTML del elemento con una plantilla de texto
-        container.innerHTML = `<h3>Producto: ${movimientos.nombre}</h3>
-                                <b> $ ${movimientos.monto}</b>`;
-        document.body.appendChild(container);
-    }
-}
-
-// Funcion Menu
-function menu () {
-    return console.log("Opciones" + "\n1) Establecer Limite de Gasto" + "\n2) Ingresar nuevo Gasto" + "\n3) Ver/Emilinar Gastos" + "\n4) Procesar Gastos" + "\n5) Mostrar Gastos" + "\n0) Salir");
-}
-
-// Algortimo Menu de opciones
-function menuOpciones () {
-
-    alert("Bienvenido al sistema de Ahorro CASH SAVER" + "\nRecuerde abrir la consola para visualizar las opciones");
     
-    menu()
-    let opc = prompt("Seleccione la opción deseada");
-    
-    while (opc != "0") {
-        switch (opc){
-            case "1" :
-                console.log("1) Establecer Limite de Gasto");
-                //Definir Limite de Gasto
-                limite = prompt("Ingrese su limite de Gasto");
-                break
-                case "2" :
-                    console.log("2) Ingresar nuevo Gasto");
-                    nvoGasto(resumen);
-                    break
-                    case "3" :
-                        console.log("3) Ver/Emilinar Gastos");
-                        verResumen (resumen);
-                        modificarResumen (resumen);
-                        break
-                        case "4" :
-                            if (limite == 0) { 
-                                alert("No se ha ingresado el limite de Gasto. Por favor, ingreselo")
-                            } else {
-                                console.log("4) Procesar Gastos");
-                                procesarGastos(resumen, resumenApr, resumenRech)
-                            }
-                            break
-                            case "5" :
-                                mostrarGastos (resumen);
-                                break 
-                                default :
-                alert("No se ha ingresado una opcion correcta")
-                break
-            }
-            menu()
-            opc = prompt("Seleccione la opción deseada");
-    }    
-    alert("Saliendo")
 }
 
-//Definir Limite de Gasto
-let limite = 0;
-let gasto = 0;
-const resumen = [];
-let resumenApr = [];
-let resumenRech = [];
+
+//panel para modificar el movimiento
+function modificarMov(mov) {
+    window.location.href = '#modMov';
+    let panelMod = document.getElementById("formularioMod");
+    panelMod.innerHTML = `<input class="panelMod" type="text" placeholder="${mov.nombre}" id="nombreMod">
+                            <input class="panelMod" type="number" placeholder="${mov.monto}"id="montoMod">
+                            <input class="botonMod" type="submit" value="Enviar">`;
+}
+
+//detectar formulario para modificar el movimiento
+let panelMod = document.getElementById("formularioMod");
+panelMod.addEventListener("submit", aplicarMod);
+let busqueda = -1;
+
+//modificar el objeto del array, actualizar el resumen y eliminar el panel
+function aplicarMod(e) {
+    e.preventDefault();
+    let inputNom = document.getElementById("nombreMod");
+    let nom = inputNom.value;
+    let inputMonto = document.getElementById("montoMod");
+    let monto = inputMonto.value;
+    resumenMovTot[flagIdMod].nombre = nom;
+    resumenMovTot[flagIdMod].monto = monto;
+    mostrarResumen();
+    deshacerPanelMod ();
+    window.location.href = '#resumen';
+}
+
+//borrar el panel para modificar movimientos
+function deshacerPanelMod () {
+    let contenedorMod = document.getElementById("formMod")
+    contenedorMod.innerHTML = "";
+    let panelMod = document.getElementById("formularioMod");
+    panelMod.innerHTML = "";
+    let tablaResultadosMod = document.getElementById("tabla2");
+    tablaResultadosMod.innerHTML = "";
+    let tableTopMod = document.getElementById("tableModTop");
+    tableTopMod.innerHTML = "";
+}
+
+//Detectar evento para crear el panel de eliminacion
+let btnDel = document.getElementById("btnEliminarMov");
+btnDel.onclick = () =>{crearPanelDel()}
+let btnDelSec = document.getElementById("btnDelSecundario");
+btnDelSec.onclick = () =>{crearPanelDel()}
+flagIdDel = -1;
+
+//crear panel de Eliminacion de movimientos
+function crearPanelDel() {
+    window.location.href = '#delMov'
+    let contenedorDel = document.getElementById("formDel")
+    contenedorDel.innerHTML = "";
+    let panelDel = document.createElement("div");
+    panelDel.innerHTML = `<input type="number" class="panelDel" id="findIdDel" placeholder="Buscar por ID">`
+    contenedorDel.append(panelDel);
+    let inputDelId = document.getElementById("findIdDel");
+    inputDelId.onchange =  () => {filtResultadosDel(inputDelId.value)}
+}
+
+
+
+//Encontrar el movimiento y mostrarlo en una tabla
+function filtResultadosDel(valor){
+    busqueda = resumenMovAct.find((el) => el.id === Number(valor));
+    let panelDel = document.getElementById("formularioDel");
+    panelDel.innerHTML = "";
+    let tableTopDel = document.getElementById("tableDelTop");
+    tableTopDel.innerHTML = "";
+    let tablaResultadosDel = document.getElementById("tabla3");
+    tablaResultadosDel.innerHTML = ""; 
+    let contenedorDel = document.getElementById("formDel")
+        contenedorDel.innerHTML = "";
+    if (resumenMovAct.includes(busqueda)) {
+        let tableTopDel = document.getElementById("tableDelTop");
+        let filaTopDel = document.createElement("tr");
+        filaTopDel.innerHTML = "";
+        filaTopDel.innerHTML = `<td>ID</td>
+                            <td>NOMBRE</td>
+                            <td>MONTO</td>`;
+        tableTopDel.append(filaTopDel);
+        let filaResultado = document.createElement("tr");
+        filaResultado.innerHTML = `<td>${busqueda.id}</td>
+                                    <td>${busqueda.nombre}</td>
+                                    <td>${busqueda.monto}</td>`;
+        tablaResultadosDel.appendChild(filaResultado);
+        confirmarEliminar(busqueda)
+    } else {
+        let contenedorDel = document.getElementById("formDel")
+        contenedorDel.innerHTML = `<p class="text-center">El valor Ingresado no existe o no es valido</p>`;
+    }
+    
+}
+
+//Confirmar la eliminacion del movimiento
+function confirmarEliminar () {
+    window.location.href = '#formularioDel';
+    let panelDel = document.getElementById("formularioDel");
+    panelDel.innerHTML  = "";
+    panelDel.innerHTML  = `<p>¿Eliminar?</p>
+                            <button id="btnSi">SI</button>
+                            <button id="btnNo">NO</button>`;
+    let botonEliminar = document.getElementById("btnSi")
+    botonEliminar.onclick = () =>{eliminarMov()} 
+    let botonNoEliminar = document.getElementById("btnNo")
+    botonNoEliminar.onclick = () =>{noEliminar()}
+}
+
+//Eliminar objeto del array, actualizar resumen y borrar panel de eliminacion
+function eliminarMov () {
+    resumenMovAct.splice(flagIdDel, 1);
+    mostrarResumen();
+    deshacerPanelDel();
+    window.location.href = '#resumen';
+}
+//borrar solamente panel de eliminacion
+function noEliminar(){
+    deshacerPanelDel();
+}
+
+//borrar panel de eliminacion
+function deshacerPanelDel() {
+    let panelDel = document.getElementById("formularioDel");
+    panelDel.innerHTML = "";
+    let tableTopDel = document.getElementById("tableDelTop");
+    tableTopDel.innerHTML = "";
+    let tablaResultadosDel = document.getElementById("tabla3");
+    tablaResultadosDel.innerHTML = ""; 
+    let contenedorDel = document.getElementById("formDel")
+    contenedorDel.innerHTML = "";
+}
+
+
+
+
+
